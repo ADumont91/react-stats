d7480a703d89249ab2aa5be2090f64ad2ca75ebc
ADumont91 <arnolddumont@gmail.com>
2020-07-24 15:02:32 +0200

    initial commit

---
+++ src/TCPage/TCPage.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { userActions } from '../_actions';
+
+class TCPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // reset login status
+        this.props.logout();
+
+        this.state = {
+            submitted: false
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        this.setState({ submitted: true });
+    }
+
+    render() {
+        const { acceptingTC } = this.props;
+        const { submitted } = this.state;
+        return (
+            <div className="col-md-6 col-md-offset-3">
+                <h2>Terms and Conditions</h2>
+				<p> We ain't liable for nothin! </p>
+                <form name="form" onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <button className="btn btn-primary">Accept</button>
+                        {acceptingTC &&
+                            <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+                        }
+                        <Link to="/register" className="btn btn-link">Back</Link>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+function mapState(state) {
+    const { acceptingTC } = state.authentication;
+    return { acceptingTC };
+}
+
+const actionCreators = {
+    login: userActions.login,
+    logout: userActions.logout
+};
+
+const connectedTCPage = connect(mapState, actionCreators)(TCPage);
+export { connectedTCPage as TCPage };
