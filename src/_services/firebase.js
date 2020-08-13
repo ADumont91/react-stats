1aa4a3fa6456efbc2d364618fdac94f6a7003ecb
ADumont91 <arnolddumont@gmail.com>
2020-08-13 23:38:50 +0200

    add: firebase config

---
+++ src/_services/firebase.js
@@ -0,0 +1,23 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from 'firebase/app';
+
+// Add the Firebase services that you want to use
+// We only want to use Firebase Auth here
+import 'firebase/auth';
+
+// Your app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCLOW8QedKhRJnqKt5yxietQAbRFfOuAxo",
+    authDomain: "react-redux-login-app.firebaseapp.com",
+    databaseURL: "https://react-redux-login-app.firebaseio.com",
+    projectId: "react-redux-login-app",
+    storageBucket: "react-redux-login-app.appspot.com",
+    messagingSenderId: "64945004156",
+    appId: "1:64945004156:web:dcacaadbfb9771c8bb73f9"
+};
+// Initialize Firebase
+//firebase.initializeApp(firebaseConfig);
+
+// Finally, export it to use it throughout your app
+export default firebase.initializeApp(firebaseConfig);
